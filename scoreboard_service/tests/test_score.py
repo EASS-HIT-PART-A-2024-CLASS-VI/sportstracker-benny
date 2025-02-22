e53ba7369012074f8188ff60952592f9846188d4
Benny Golber <benigolber25@gmail.com>
2025-02-22 12:50:17 +0200

    add pytest fixture

--- scoreboard_service/tests/test_score.py
+++ scoreboard_service/tests/test_score.py
@@ -4,6 +4,7 @@ import httpx
 @pytest.fixture(scope="session")
 def scoreboard_client():
     return httpx.Client(base_url="http://scoreboard_service:8000")
+@pytest.fixture(scope="session")
 def management_client():
     return httpx.Client(base_url="http://management_service:8000")
 
